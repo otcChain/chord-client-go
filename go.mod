f36df00670d870e8db14c177bedd3007fb7e74c2
hyperorchid <wsli@192.168.0.101>
2021-03-07 00:09:32 +0800

    first test case

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/otcChain/chord-client-go
 
 go 1.16
+
+require github.com/otcChain/chord-go v0.0.0-20210306054507-f43dde62d605
--- main.go
+++ main.go
@@ -1,5 +1,65 @@
 package main
+import (
+	"context"
+	"fmt"
+	"github.com/herumi/bls-eth-go-binary/bls"
+	"log"
+	"math/big"
+
+	"github.com/otcChain/chord-go/chain/types"
+	"github.com/otcChain/chord-go/common"
+	"github.com/otcChain/chord-go/rpc/rpc_client"
+)
 
 func main() {
 
+	client, err := chordclient.Dial("http://127.0.0.1:6666")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var privateKey bls.SecretKey
+	err = privateKey.DeserializeHexStr("066c6b1a28955a9089670d1e1386484f7370ef7b4f725876e72d82438de06c9e")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	publicKey := privateKey.GetPublicKey()
+
+	fromAddress := common.PubKeyToAddr(publicKey)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	value := big.NewInt(1000000000000000000) // in wei (1 eth)
+	gasLimit := uint64(21000)                // in units
+
+	toAddress, err := common.HexToAddress("fed1gy3afwa745c88dxsznaw82trul3r2p5vsrhmms")
+	var data []byte
+	//TODO:: make sure the usage of chainID
+	//chainID, err := client.NetworkID(context.Background())
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+
+	ltx := types.TxData{
+		Nonce:    nonce,
+		To:       &toAddress,
+		Value:    value,
+		Gas:      gasLimit,
+		Data:     data,
+		Price:nil,
+	}
+	tx := types.NewTx(ltx)
+
+	if err := tx.SignTx(privateKey); err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.SendTransaction(context.Background(), tx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("tx sent: %s", tx.Hash().Hex())
 }
